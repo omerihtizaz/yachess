2464514d7cee5382236e36e5f92a67edac6154c0
Devin Alvaro <devin.alvaro@gmail.com>
2017-05-12 21:01:38 +0700

    Add self.depth

--- src/computer.py
+++ src/computer.py
@@ -1,4 +1,6 @@
 class Computer:
+    depth = 3
+
     def __init__(self, board, is_player_white):
         self.board = board
         self.is_computer_white = not is_player_white
@@ -10,7 +12,7 @@ class Computer:
         for move in self.board.legal_moves():
             self.board.push(move)
 
-            local_score = self.minimax(3, self.is_computer_white, -1e8, 1e8)
+            local_score = self.minimax(self.depth, self.is_computer_white, -1e8, 1e8)
 
             if self.is_computer_white and local_score > global_score:
                 global_score = local_score
